1d568255f8744ad500046575171b964c7f9b5235
idk <idk@epicmail.totallyreal>
2025-10-31 12:32:37 +0300

    Доубирал лишнюю страницу

--- rv-front/src/App.tsx
+++ rv-front/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { MainPage } from './pages/MainPage';
 import CommandsPage from './pages/CommandsPage';
 import CommandInfoPage from './pages/CommandInfoPage';
-import ProgramPage from './pages/ProgramPage'
 import { ROUTES } from './Routes';
 import { type FC } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -15,7 +14,6 @@ const App: FC = () => {
           <Route path={ROUTES.HOME} element={<MainPage />} />
           <Route path={ROUTES.COMMANDS} element={<CommandsPage />} />
           <Route path={ROUTES.COMMANDS_INFO} element={<CommandInfoPage />} />
-          <Route path={ROUTES.PROGRAM} element={<ProgramPage />} />
         </Routes>
       </div>
     </BrowserRouter>
