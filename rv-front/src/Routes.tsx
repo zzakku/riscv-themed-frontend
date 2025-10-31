965194afbab59f895c784e38c953ffa87913746b
idk <idk@epicmail.totallyreal>
2025-10-31 10:46:36 +0300

    Убрал лишнюю страницу

--- rv-front/src/Routes.tsx
+++ rv-front/src/Routes.tsx
@@ -2,12 +2,10 @@ export const ROUTES = {
   HOME: "/",
   COMMANDS: "/commands",
   COMMANDS_INFO: "/commands/:id",
-  PROGRAM: "/programs/:id"
 }
 export type RouteKeyType = keyof typeof ROUTES;
 export const ROUTE_LABELS: {[key in RouteKeyType]: string} = {
   HOME: "Главная",
   COMMANDS: "Команды",
-  COMMANDS_INFO: "Описание команды",
-  PROGRAM: "Программа"
+  COMMANDS_INFO: "Описание команды"
 };
