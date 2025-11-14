7fdec3a3fd596540a509054dd6c4ecb4d06b888a
idk <idk@epicmail.totallyreal>
2025-11-14 05:18:51 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/riscv-themed-frontend/sw.js', { scope: '/riscv-themed-frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
